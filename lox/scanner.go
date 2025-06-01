a92db4f6538600d58538393b79141edbc9398046
Per Næss <perness3009@gmail.com>
2025-06-01 13:55:32 +0200

    feat: add scantokens 4.5

--- lox/scanner.go
+++ lox/scanner.go
@@ -1,5 +1,7 @@
 package lox
 
+import "fmt"
+
 type Scanner struct {
 	source  string
 	tokens  []Token
@@ -27,4 +29,43 @@ func (s *Scanner) scanTokens() []Token {
 
 func (s *Scanner) isAtEnd() bool {
 	return s.current >= len(s.source)
+}
+
+func (s *Scanner) scanToken() {
+	s.advance()
+	switch s.source[s.current] {
+	case '(':
+		s.addToken(LEFT_PAREN)
+	case ')':
+		s.addToken(RIGHT_PAREN)
+	case '{':
+		s.addToken(LEFT_BRACE)
+	case '}':
+		s.addToken(RIGHT_BRACE)
+	case ',':
+		s.addToken(COMMA)
+	case '.':
+		s.addToken(DOT)
+	case '-':
+		s.addToken(MINUS)
+	case '+':
+		s.addToken(PLUS)
+	case ';':
+		s.addToken(SEMICOLON)
+	case '*':
+		s.addToken(STAR)
+	}
+}
+
+func (s *Scanner) advance() {
+	s.current++
+}
+
+func (s *Scanner) addToken(tokenType TokenType) {
+	s.addTokenWithLiteral(tokenType, nil)
+}
+
+func (s *Scanner) addTokenWithLiteral(tokenType TokenType, literal fmt.Stringer) {
+	text := s.source[s.start:s.current]
+	s.tokens = append(s.tokens, Token{tokenType, text, literal, s.line})
 }
